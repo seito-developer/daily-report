40a0f8ca72e55e9c6cafc9f51e0dff7e2bef1639
seito-developer <seito.horiguchi@gmail.com>
2024-01-06 02:38:05 +0900

    changed html tags

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-import 'sanitize.css';
-import './style.css';
+(() => {
+
+    document.getElementById("js-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        formData.forEach((value, key) => {
+            console.log(key, value);
+        });
+    });
+
+})();
+
